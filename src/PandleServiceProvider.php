abb5055d7fcb864214e3d510615067879a05ff88
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2025-01-17 13:29:02 +0000

    Initial commit

---
+++ src/PandleServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aybarsm\Laravel\Pandle;
+
+use Illuminate\Support\ServiceProvider;
+
+final class PandleServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
